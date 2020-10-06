7782c0f39df53caf234961b72293a60890469ceb
Micah <micalobia2@gmail.com>
2020-10-06 05:56:31 -0700

    Updated setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="discordqueue-Micalobia",  # Replace with your own username
+    name="discordqueue",
     version="0.1.0",
     author="Micalobia",
     author_email="micalobiabusiness@gmail.com",
